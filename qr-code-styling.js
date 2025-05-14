b2e6dbe9f26f73d5f1d6a911b3195406a086b3cc
Aureum (AUR) <us.dmassaro@gmail.com>
2025-05-14 11:20:23 -0400

    Update qr-code-styling.js

--- qr-code-styling.js
+++ qr-code-styling.js
@@ -1,1 +1,2 @@
 // Placeholder: Use qr-code-styling from https://github.com/kozakdenys/qr-code-styling
+<script src="https://cdn.jsdelivr.net/npm/qr-code-styling@1.5.0/lib/qr-code-styling.js"></script>
